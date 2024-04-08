553a7106a14a1bc7ff25bc17f8a64ca81b62f7bc
demjened <demjened@gmail.com>
2024-04-07 20:31:52 -0400

    Run script

--- api/app.py
+++ api/app.py
@@ -4,10 +4,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/api/login', methods=["POST"])
 def flask_login():
     request_body_json = request.get_json()
@@ -21,4 +17,4 @@ def flask_login():
     return jsonify({'success': True, 'email': email})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
